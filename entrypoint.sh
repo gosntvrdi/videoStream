b230ce9730ffaa2970433e308c156c93ec441455
gosntvrdi <father.luka@gmail.com>
2019-02-15 11:51:41 +0100

    s

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
 
-echo starting to run runtime_properties...
 python /app.py
-echo finished runtime_properties
-
-
-
-echo starting to run runtime_properties...
 python /videoScheduler.py
-echo finished runtime_properties
