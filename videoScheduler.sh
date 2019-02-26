bb83ab2d4416184315df977769e67e70bab19acf
gosntvrdi <39313008+gosntvrdi@users.noreply.github.com>
2019-02-26 20:43:39 +0100

    Add files via upload

---
+++ videoScheduler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt install python-pip
+sudo apt-get install wmctrl
+sudo apt-get install xdotool
+cd /app
+pip install -r requirements.txt
+python videoPlayer.py &
+python videoPlaylist.py &
+python app.py
+sleep 3
+wmctrl -k on
+sleep 10
+xdg-open http://127.0.0.1:5000
+sleep 5
+xdotool key F11
